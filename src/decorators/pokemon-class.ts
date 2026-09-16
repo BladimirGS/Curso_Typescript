9f5920f051d70e038cc5c630f0c1de408777e6da
BladimirGS <hugobladimirgs@gmail.com>
2026-09-15 22:04:06 -0600

    Factory decorators

---
+++ src/decorators/pokemon-class.ts
@@ -0,0 +1,20 @@
+function printToConsole( constructor: Function ) {
+  console.log(constructor)
+}
+
+const printToConsoleConditional = (print: boolean): Function => {
+  if(print) {
+    return printToConsole
+  } else {
+    return () => {}
+  }
+}
+
+@printToConsoleConditional(false)
+export class Pokemon {
+  public publicApi = 'https//pokeapi.co'
+
+  constructor(
+    public name: string
+  ) {}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { getPokemon } from "./generics/get-pokemon";
+import { Pokemon } from "./decorators/pokemon-class";
 
-getPokemon(4)
-  .then( resp => console.log(resp) )
-  .catch( error => console.error(error) )
-  .finally( () => console.log('Fin de getPokemon') )
+
+
+const charmander = new Pokemon('Charmander');
+
+console.log(charmander)
+
